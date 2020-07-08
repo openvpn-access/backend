b973b8c6807cc6c1bfccdc3350d470c538394e2a
Nathan Seymour <nathan@seymour.global>
2020-07-08 16:21:21 +0200

    Add database configuration

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,19 +5,23 @@
 connect_to_database() {
     echo "Configuring the database..."
     while true; do
-        mysql --host=db --user=root --password=$1 -t -e "SELECT VERSION();"
+        mysql -u openvpn-access -p${1} -h db -e "SELECT VERSION();" openvpn_access
         if [[ $? -eq 0 ]]; then
             break
         else
             echo "Database not available, trying again in 1 second..."
-            sleep 1
+            sleep 5
         fi
     done
 }
 
-connect_to_database
+connect_to_database $1
 
 echo "Connected to database!"
+
+echo "Configuring database..."
+
+mysql -u openvpn-access -p${1} -h db openvpn_access < /opt/app/sql/Setup.sql
 
 # Start app
 
