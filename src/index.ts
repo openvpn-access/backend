0ee9f9150c00501abaa09603563de33f9db333c5
Simon Reinisch <simon@reinisch.io>
2020-08-13 21:19:11 +0200

    Connect to database immediately on startup

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,41 @@ import cors from 'cors';
 import express from 'express';
 import {api} from './api';
 import {config} from './config';
+import {db} from './db';
 import {log, LogLevel} from './logging';
 
-const env = process.env.NODE_ENV;
+// Used in tests
 export const app = express();
 
-// Disable powered-by-message
-app.disable('x-powered-by');
-app.set('trust proxy', true);
-app.use(bodyParser.json());
+(async () => {
+    const env = process.env.NODE_ENV;
 
-if (env === 'development') {
+    // Disable powered-by-message
+    app.disable('x-powered-by');
+    app.set('trust proxy', true);
+    app.use(bodyParser.json());
 
-    // Enable cors during development
-    app.use(cors());
-}
+    if (env === 'development') {
 
-// Register api
-app.use('/api/v1', api());
+        // Enable cors during development
+        app.use(cors());
+    }
 
-if (env !== 'test') {
-    log('booting', {
-        message: `Starting app in ${env}`
-    }, LogLevel.INFO);
+    // Register api
+    app.use('/api/v1', api());
 
-    app.listen(config.server.port);
-    log('booting', {
-        message: 'Server successfully started launched.'
-    }, LogLevel.INFO);
-}
+    // Connect to database immediately
+    await db.$connect();
+    log('booting', {message: 'Connected to database'}, LogLevel.INFO);
+
+    if (env !== 'test') {
+        log('booting', {
+            message: `Starting app in ${env}`
+        }, LogLevel.INFO);
+
+        app.listen(config.server.port);
+        log('booting', {
+            message: 'Server successfully started launched.'
+        }, LogLevel.INFO);
+    }
+})();
